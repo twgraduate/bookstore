0dc3874ddad4df12936047a29aea1a0872e6f400
rhuanhuan <1210098674@qq.com>
2016-08-09 14:51:57 +0800

    Delete all things about database and ActiveRecord in my project

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
+require 'all'
 
-require 'rails/all'
+require "action_controller/railtie"
+require 'action_view/railtie'
+require "action_mailer/railtie"
+require 'action_cable/engine'
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
+# require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
